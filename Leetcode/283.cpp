90a664dcb7d14295f09a3b22ee51ef88bec6ad09
zhongchong <41172680@qq.com>
2015-09-25 11:21:38 +0800

    添加283.cpp

---
+++ Leetcode/283.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeroPos = 0;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums.at(i) != 0 )
+            {
+                if(i != zeroPos)
+                    swap(nums.at(i), nums.at(zeroPos) );
+                ++zeroPos;
+            }
+        }
+    }
+};
