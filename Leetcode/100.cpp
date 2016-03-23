6221146fa8c191eb4fcffc92e9bee5ee5cf00ef7
Zhong Chong <zhongchong@users.noreply.github.com>
2016-03-23 13:35:06 +0800

    Added files via upload

---
+++ Leetcode/100.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr)
+            return true;
+        else if(p != nullptr && q!= nullptr )
+        {
+            if(p->val != q->val)
+                return false;
+            else
+            {
+                if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right))
+                    return true;
+                return false;
+            }
+        }
+        else
+            return false;
+    }
+};
